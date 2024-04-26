af02bdb5efb4a54e531d76efa2ccd00d1c82c567
Fahim <faahim06@gmail.com>
2024-04-26 19:33:41 +0600

    latest file

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-// Check if setup has already been completed
+
 if (file_exists('setup_completed.flag')) {
     echo "Setup has already been completed. ";
 } else {
@@ -7,15 +7,15 @@ if (file_exists('setup_completed.flag')) {
     define('DB_USER', 'root');
     define('DB_PASS', '');
 
-    // Create Connection
+    
     $link = new mysqli(DB_HOST, DB_USER, DB_PASS);
 
-    // Check Connection
+    
     if ($link->connect_error) {
         die('Connection Failed: ' . $link->connect_error);
     }
 
-    // Create the 'restaurantdb' database if it doesn't exist
+    
     $sqlCreateDB = "CREATE DATABASE IF NOT EXISTS restaurantdb";
     if ($link->query($sqlCreateDB) === TRUE) {
         echo "Database 'restaurantdb' created successfully.<br>";
@@ -23,27 +23,27 @@ if (file_exists('setup_completed.flag')) {
         echo "Error creating database: " . $link->error . "<br>";
     }
 
-    // Switch to using the 'restaurantdb' database
+    
     $link->select_db('restaurantdb');
 
-    // Execute SQL statements from "restaurantdb.txt"
+    
     function executeSQLFromFile($filename, $link) {
         $sql = file_get_contents($filename);
 
-        // Execute the SQL statements
+        
         if ($link->multi_query($sql) === TRUE) {
             echo "SQL statements executed successfully.";
-            // Set the flag to indicate setup is complete
+            
             file_put_contents('setup_completed.flag', 'Setup completed successfully.');
         } else {
             echo "Error executing SQL statements: " . $link->error;
         }
     }
 
-    // Execute SQL statements from "restaurantdb.txt"
+    
     executeSQLFromFile('restaurantdb.txt', $link);
 
-    // Close the database connection
+    
     $link->close();
 }
 ?>
